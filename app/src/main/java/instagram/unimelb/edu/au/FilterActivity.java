1a2633c7e68e9816d27768928716458c13c1baa8
Angela Carrera <carreraa@s>
2015-10-06 09:44:58 +1100

    Share button

--- app/src/main/java/instagram/unimelb/edu/au/FilterActivity.java
+++ app/src/main/java/instagram/unimelb/edu/au/FilterActivity.java
@@ -1,5 +1,8 @@
 package instagram.unimelb.edu.au;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -7,7 +10,10 @@ import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -23,6 +29,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Locale;
 
 import instagram.unimelb.edu.au.photo.CapturePreview;
 
@@ -184,6 +191,8 @@ public class FilterActivity extends AppCompatActivity {
      */
     private void SaveFilteredImage(Bitmap image){
         File pictureFile;
+        String type = "image/*";
+       /* Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_MOUNTED);*/
         pictureFile = CapturePreview.getOutputMediaFile(CapturePreview.MEDIA_TYPE_IMAGE);
         if (pictureFile == null) {
             Log.d(TAG,"Error creating media file, check storage permissions: ");// e.getMessage());
@@ -194,12 +203,51 @@ public class FilterActivity extends AppCompatActivity {
             image.compress(Bitmap.CompressFormat.PNG, 90, fos);
             fos.close();
             Toast.makeText(FilterActivity.this, "Photo Saved", Toast.LENGTH_SHORT).show();
+
+            /*
+            Adding information to the gallery
+             */
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.TITLE, pictureFile.getName());
+            values.put(MediaStore.Images.Media.DESCRIPTION,pictureFile.getName());
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis ());
+            values.put(MediaStore.Images.ImageColumns.BUCKET_ID, pictureFile.toString().toLowerCase(Locale.US).hashCode());
+            values.put(MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME, pictureFile.getName().toLowerCase(Locale.US));
+            values.put("_data", pictureFile.getAbsolutePath());
+
+            ContentResolver cr = getContentResolver();
+            cr.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            /*
+             Calling the share intent to Instagram app
+            */
+            createInstagramIntent(type,pictureFile.getPath());
         } catch (FileNotFoundException e) {
             Log.d(TAG, "File not found: " + e.getMessage());
         } catch (IOException e) {
             Log.d(TAG, "Error accessing file: " + e.getMessage());
         }
 
+    }
+    /*
+    Instagram intent
+     */
+    private void createInstagramIntent(String type, String mediaPath){
+
+        // Create the new Intent using the 'Send' action.
+        Intent share = new Intent(Intent.ACTION_SEND);
+
+        // Set the MIME type
+        share.setType(type);
+
+        // Create the URI from the media
+        File media = new File(mediaPath);
+        Uri uri = Uri.fromFile(media);
+
+        // Add the URI to the Intent.
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+
+        // Broadcast the Intent.
+        startActivity(Intent.createChooser(share, "Share to"));
     }
     public static Bitmap RotateBitmap(Bitmap source, float angle)
     {
