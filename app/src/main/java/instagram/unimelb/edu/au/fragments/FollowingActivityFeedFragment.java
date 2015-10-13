9ec579d7a6ce83e3e27e9df69a9b80e14ec85604
Angela Carrera <carreraa@s>
2015-10-13 19:57:05 +1100

    comments

--- app/src/main/java/instagram/unimelb/edu/au/fragments/FollowingActivityFeedFragment.java
+++ app/src/main/java/instagram/unimelb/edu/au/fragments/FollowingActivityFeedFragment.java
@@ -19,12 +19,7 @@ import instagram.unimelb.edu.au.models.FollowingActivityFeed;
 import instagram.unimelb.edu.au.utils.Globals;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FollowingActivityFeedFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FollowingActivityFeedFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Following Activity FeedFragment
  */
 public class FollowingActivityFeedFragment extends Fragment {
     private static final String ARG_ACCESSTOKEN = "param1";
@@ -44,21 +39,17 @@ public class FollowingActivityFeedFragment extends Fragment {
     public FollowingActivityFeedFragment followingActivityFragment;
     Boolean userScrolled = false;
 
-    //Integer nposts = 38;
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FollowingActivityFeedFragment.
+     * New Fragment Instance
+     * @param accessToken
+     * @param clientId
+     * @return
      */
-    // TODO: Rename and change types and number of parameters
-    public static FollowingActivityFeedFragment newInstance(String param1, String param2) {
+    public static FollowingActivityFeedFragment newInstance(String accessToken, String clientId) {
         FollowingActivityFeedFragment fragment = new FollowingActivityFeedFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_ACCESSTOKEN, param1);
-        args.putString(ARG_CLIENTID, param2);
+        args.putString(ARG_ACCESSTOKEN, accessToken);
+        args.putString(ARG_CLIENTID, clientId);
         fragment.setArguments(args);
         return fragment;
     }
@@ -102,7 +93,10 @@ public class FollowingActivityFeedFragment extends Fragment {
         return rootView;
     }
 
-
+    /**
+     * Add follower profile media to the adapter
+     * @param follower
+     */
 
     public void addProfileMedia(FollowingActivityFeed follower) {
 
@@ -126,7 +120,7 @@ public class FollowingActivityFeedFragment extends Fragment {
                 if (firstVisibleItem + visibleItemCount >= totalItemCount && userScrolled && currentFirstVisPos > myLastVisiblePos && Globals.FOLLOWERACTIVITY_MEDIA_MAX_ID != "-1") {
                     //Toast.makeText(getActivity(), "reach bottom", Toast.LENGTH_SHORT).show();
                     userScrolled = false;
-                    objFollowers.getProfileMedia(followingActivityFragment, mParam1, mParam2,gridAdapter);
+                    objFollowers.getProfileMedia(followingActivityFragment, mParam1, mParam2, gridAdapter);
                 }
                 myLastVisiblePos = currentFirstVisPos;
             }
@@ -134,7 +128,6 @@ public class FollowingActivityFeedFragment extends Fragment {
     }
 
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
